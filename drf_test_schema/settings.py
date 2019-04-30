dd6623a2232737d5697d962595d86bc16ddcda7b
Sam Hartsfield <sam.hartsfield@acspower.com>
2019-04-30 13:29:28 -0400

    Enable OpenAPI schema generation

--- drf_test_schema/settings.py
+++ drf_test_schema/settings.py
@@ -125,5 +125,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_METADATA_CLASS': 'rest_framework.schemas.openapi.AutoSchema',
 }
