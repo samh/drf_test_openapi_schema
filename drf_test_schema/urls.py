ae15ea546663292d564da9adf2132b963c3a6a11
Sam Hartsfield <sam.hartsfield@acspower.com>
2019-05-01 11:12:16 -0400

    Added test model

--- drf_test_schema/urls.py
+++ drf_test_schema/urls.py
@@ -18,20 +18,37 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+from sample1.models import Thing
+
+
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
+
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class ThingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thing
+        fields = ('name', 'number', 'interval', 'value')
+
+
+class ThingViewSet(viewsets.ModelViewSet):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'things', ThingViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
--- sample1/models.py
+++ sample1/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+INTERVAL_CHOICES = (
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+    (6, "6"),
+    (10, "10"),
+    (12, "12"),
+    (15, "15"),
+    (20, "20"),
+    (30, "30"),
+    (60, "60"),
+)
+
+VALUE_CHOICES = (("F", "Foo"), ("B", "Bar"), ("BZ", "Baz"))
+
+
+class Thing(models.Model):
+    name = models.CharField(
+        "thing name",
+        help_text="The name of the thing. This should be unique.",
+        max_length=200,
+        unique=True,
+    )
+
+    number = models.PositiveIntegerField(
+        default=2,
+        help_text="This has a defined default, min, and max value",
+        validators=[MinValueValidator(0), MaxValueValidator(1500)],
+    )
+
+    interval = models.PositiveIntegerField(
+        "interval (minutes)", default=60, choices=INTERVAL_CHOICES
+    )
+
+    value = models.CharField(max_length=10, choices=VALUE_CHOICES, default="B")
